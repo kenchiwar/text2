431172ad4b31a01d66a93f1ba1b4bdca11c22672
kenchiwar <huynhtienthang1264@gmail.com>
2023-02-05 21:00:36 +0700

    hay c5

---
+++ huynh_tien_thang_c2109i2_25_1/Class4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace huynh_tien_thang_c2109i2_25_1
+{
+    internal class Class4
+    {
+    }
+}
